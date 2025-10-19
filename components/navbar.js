acc3763098a26c8cfd3c78503f6fccf85a0f9304
daniel holguin <d.holguin@protonmail.com>
2025-10-18 23:05:55 -0700

    update icon path

--- components/navbar.js
+++ components/navbar.js
@@ -25,7 +25,7 @@ class Navbar extends HTMLElement {
       </style>
 
       <nav>
-        <img src="../assets/aang-32.png" alt="Aang logo" />
+        <img src="./assets/aang-32.png" alt="Aang logo" />
         <a href="/" data-link>Home</a>
         <a href="/about" data-link>About</a>
       </nav>
